52715906d368e7ed9059a3e284ef5745fdd33439
Yosua Martiansia <yosuamartiansia@gmail.com>
2020-03-17 16:45:55 +0700

    Added Git Ignore

--- build/src/CMakeFiles/Index_Buffer.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libIndex_Buffer.a"
-)
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yosua/Projects/OpenGL/tests
-# Build directory: /home/yosua/Projects/OpenGL/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
